2c03566940be60f1fd9a85df2b9f80da46c7398f
nadinewahdan <nadine.a.wahdan@gmail.com>
2015-12-15 13:20:26 +0200

    sala7t model follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,6 @@
 class Follow < ActiveRecord::Base
+	 belongs_to :follower, :class_name => 'User'
+    belongs_to :followed, :class_name => 'User'
+    
+    validates :follower_id, uniqueness => {:scope => :followed_id}
 end
